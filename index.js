4571d2313a7d1efce59292b8a57c3f9c5074409a
karinavarlena <karina.varlena@gmail.com>
2020-05-11 22:34:24 +0300

    add classes

--- index.js
+++ index.js
@@ -2,23 +2,6 @@ const API_URL = 'https://api.github.com/';
 const rootElement = document.getElementById('root');
 const loadingElement = document.getElementById('loading-overlay');
 const fightersDetailsMap = new Map();
-
-async function startApp() {
-    try {
-      loadingElement.style.visibility = 'visible';
-      
-      const endpoint = 'repos/sahanr/street-fighter/contents/fighters.json';
-      const fighters = await callApi(endpoint, 'GET');
-      const fightersElement = createFighters(fighters);
-
-      rootElement.appendChild(fightersElement);
-    } catch (error) {
-      console.warn(error);
-      rootElement.innerText = 'Failed to load data';
-    } finally {
-      loadingElement.style.visibility = 'hidden';
-    }
-}
   
 function callApi(endpoind, method) {
     const url = API_URL + endpoind
@@ -32,75 +15,125 @@ function callApi(endpoind, method) {
             ? response.json() 
             : Promise.reject(Error('Failed to load'))
         )
-        .then(file => JSON.parse(atob(file.content)))
         .catch(error => { throw error });
 }
 
-function createElement({ tagName, className = '', attributes = {} }) {
-    const element = document.createElement(tagName);
-    element.classList.add(className);
-        
-    Object
-        .keys(attributes)
-        .forEach(key => element.setAttribute(key, attributes[key]));
-
-    return element;
-}
-
-function createName(name) {
-    const nameElement = createElement({ tagName: 'span', className: 'name' });
-    nameElement.innerText = name;
+class FighterService {
+    async getFighters() {
+      try {
+        const endpoint = 'repos/sahanr/street-fighter/contents/fighters.json';
+        const apiResult = await callApi(endpoint, 'GET');
   
-    return nameElement;
-}
-  
-function createImage(source) {
-    const attributes = { src: source };
-    const imgElement = createElement({
-      tagName: 'img',
-      className: 'fighter-image',
-      attributes
-    });
-  
-    return imgElement;
-}
-
-function createFighter(fighter) {
-    const { name, source } = fighter;
-    const nameElement = createName(name);
-    const imageElement = createImage(source);
-    const element = createElement({ tagName: 'div', className: 'fighter' });
-       
-    element.addEventListener('click', (event) => handleFighterClick(event, fighter), false)
-    
-    element.append(imageElement, nameElement);
-  
-    return element;
- }
-
-function createFighters(fighters) {
-    const fighterElements = fighters.map(fighter => createFighter(fighter));
-    const element = createElement({ tagName: 'div', className: 'fighters' });
-  
-    element.append(...fighterElements);
-  
-    return element;
-}
-
-function handleFighterClick(event, fighter) {
-    const { _id } = fighter;
-  
-    if(!fightersDetailsMap.has(_id)) {
-      // send request here
-      fightersDetailsMap.set(_id, fighter);
+        return JSON.parse(atob(apiResult.content));
+      } catch (error) {
+        throw error;
+      }
     }
+  }
   
-    console.log(fightersDetailsMap.get(_id));
+  const fighterService = new FighterService();
+
+  class View {
+    element;
+  
+    createElement({ tagName, className = '', attributes = {} }) {
+      const element = document.createElement(tagName);
+      element.classList.add(className);
+      
+      Object.keys(attributes).forEach(key => element.setAttribute(key, attributes[key]));
+  
+      return element;
+    }
   }
 
-function getFightersNames(fighters) {
-  const names = fighters.map(it => it.name).join('\n');
-  return names;
-}
+  class FighterView extends View {
+    constructor(fighter, handleClick) {
+      super();
+  
+      this.createFighter(fighter, handleClick);
+    }
+  
+    createFighter(fighter, handleClick) {
+      const { name, source } = fighter;
+      const nameElement = this.createName(name);
+      const imageElement = this.createImage(source);
+  
+      this.element = this.createElement({ tagName: 'div', className: 'fighter' });
+      this.element.append(imageElement, nameElement);
+      this.element.addEventListener('click', event => handleClick(event, fighter), false);
+    }
+  
+    createName(name) {
+      const nameElement = this.createElement({ tagName: 'span', className: 'name' });
+      nameElement.innerText = name;
+  
+      return nameElement;
+    }
+  
+    createImage(source) {
+      const attributes = { src: source };
+      const imgElement = this.createElement({
+        tagName: 'img',
+        className: 'fighter-image',
+        attributes
+      });
+  
+      return imgElement;
+    }
+  }
+  
+  class FightersView extends View {
+    constructor(fighters) {
+      super();
+      
+      this.createFighters(fighters);
+    }
+  
+    fightersDetailsMap = new Map();
+  
+    createFighters(fighters) {
+      const fighterElements = fighters.map(fighter => {
+        const fighterView = new FighterView(fighter, this.handleFighterClick);
+        return fighterView.element;
+      });
+  
+      this.element = this.createElement({ tagName: 'div', className: 'fighters' });
+      this.element.append(...fighterElements);
+    }
+  
+    handleFighterClick(event, fighter) {
+      this.fightersDetailsMap.set(fighter._id, fighter);
+      console.log('clicked')
+      // get from map or load info and add to fightersMap
+      // show modal with fighter info
+      // allow to edit health and power in this modal
+    }
+  }
 
-startApp();
+  class App {
+    constructor() {
+      this.startApp();
+    }
+  
+    static rootElement = document.getElementById('root');
+    static loadingElement = document.getElementById('loading-overlay');
+  
+    async startApp() {
+      try {
+        App.loadingElement.style.visibility = 'visible';
+        
+        const fighters = await fighterService.getFighters();
+        const fightersView = new FightersView(fighters);
+        const fightersElement = fightersView.element;
+  
+        App.rootElement.appendChild(fightersElement);
+      } catch (error) {
+        console.warn(error);
+        App.rootElement.innerText = 'Failed to load data';
+      } finally {
+        App.loadingElement.style.visibility = 'hidden';
+      }
+    }
+  }
+  
+  new App();
