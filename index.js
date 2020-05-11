fa598349bbb06732a7e89a28943d08550fda34f6
karinavarlena <karina.varlena@gmail.com>
2020-05-11 21:57:50 +0300

    add loading overlay

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
 const API_URL = 'https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json';
-const root = document.getElementById("root");
-root.innerText = "Loading...";
-console.log(fetch(API_URL))
+const rootElement = document.getElementById('root');
+const loadingElement = document.getElementById('loading-overlay');
+
+rootElement.innerText = 'Loading...';
 
 fetch(API_URL)
-.then((response) => response.json())
-.then(file => {
-    console.log(file);
+    .then(response => {
+        if (!response.ok) {
+            return Promise.reject(new Error('Failed load data'));
+        }
+        return response.json();
+    })
+  .then(file => {
     const fighters = JSON.parse(atob(file.content));
-    console.log(fighters);
-    
-    let names = fighters.map(fighter => {
-        console.log(fighter.name);
-        return fighter.name;
-    }).join('\n');
-    root.innerText = names;
-})
-.catch((err) => {
-    console.error(err.message);
-});
-
-// let myPromise = new Promise(
-//     function(resolve, reject) {
-//         setTimeout(() => {
-//             console.log("pause is end");
-//             reject();
-//         }, 5000);
-//     }
-// );
-
-// myPromise
-// .then(function() {
-//     console.log("async code is done!!! It's successed!!");
-// })
-// .catch(function() {
-//     console.error("Oupss. It's error.");
-// })
-
-// console.log(myPromise);
+    const names = fighters.map(it => it.name).join('\n');
+    rootElement.innerText = names;
+  })
+  .catch(error => {
+    console.warn(error);
+    root.innerText = 'Failed to load data';
+  })
+  .finally(() => {
+    loadingElement.remove();
+  })
